92931fe039c36b396ada334ecdbdefdc230ee2b9
SKSKIe <gahee3074@gmail.com>
2023-07-08 15:46:08 +0900

    Update trial.js

--- commands/trial.js
+++ commands/trial.js
@@ -1,4 +1,5 @@
 const { ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, ContextMenuCommandBuilder, ApplicationCommandType } = require('discord.js');
+const axios = require('axios');
 let vton = false;
 module.exports = {
 	  data: new ContextMenuCommandBuilder()
@@ -33,7 +34,7 @@ module.exports = {
 	      .addFields(
           { name: `\u200B`, value: `해당 채팅에 대한 약식 재판을 시작합니다.\nhttps://discord.com/channels/${msg.guild.id}/${msg.channelId}/${msg.id}` },
 )
-        .setFooter({ text: '1분 후에 재판의 결과가 발표됩니다.' })
+        .setFooter({ text: '1분 30초 후에 재판의 결과가 발표됩니다.' })
         .setTimestamp();
       const row = new ActionRowBuilder()
         .addComponents(
@@ -54,7 +55,7 @@ module.exports = {
 
       const collector = interaction.channel.createMessageComponentCollector({
         //몇 초동안 반응 할 수 있는지
-        time: 100000
+        time: 90000
       });
       collector.on('collect', async (interaction) => {
         if(vtMember.indexOf(interaction.user.id) != -1){ // 버튼 눌렀을때
@@ -87,9 +88,19 @@ module.exports = {
           .setTimestamp();
 
 				  if(vtA > vtB && vtA > 2){
-            exampleEmbed.addFields({name: ' ' , value:`\`\`해당 채팅에 대한 처벌이 가결되었습니다.\`\`\nhttps://discord.com/channels/${msg.guild.id}/${msg.channelId}/${msg.id}`})
             let member = await interaction.guild.members.fetch(msg.author.id);
-            
+            let credit = Number((vtA-1)*(-1));
+            exampleEmbed.addFields({name: ' ' , value:`\`\`해당 채팅에 대한 처벌이 가결되었습니다. (소셜 크레딧 ${credit*(-1)} 차감)\`\`\nhttps://discord.com/channels/${msg.guild.id}/${msg.channelId}/${msg.id}`})
+            try {
+              axios.post('https://port-0-api-otjl2clhuho5p6.sel4.cloudtype.app/initialize', { id: msg.author.id, credit }, {
+                  headers: {
+                      'Authorization': 'QWl0c3VraSBOYWt1cnU',
+                      'Content-Type': 'application/json'
+                  }
+              })
+            } catch (error) {
+              exampleEmbed.addFields({name: ' ' , value:`\`\`크레딧 차감에 문제가 있는 대상입니다.\`\``});
+            }
             try {
               await member.timeout(60000 * 3.5); // 1분 30초 동안 타임아웃
             } catch (error) {
