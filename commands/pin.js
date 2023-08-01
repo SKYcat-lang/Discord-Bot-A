bd2eb501992696849a18ffbe4a6cf3b413cda1d7
SKSKIe <gahee3074@gmail.com>
2023-08-01 22:30:59 +0900

    Update pin.js

--- commands/pin.js
+++ commands/pin.js
@@ -1,4 +1,4 @@
-const { EmbedBuilder, ContextMenuCommandBuilder, ApplicationCommandType } = require('discord.js');
+const { ContextMenuCommandBuilder, ApplicationCommandType } = require('discord.js');
 module.exports = {
 	  data: new ContextMenuCommandBuilder()
       .setName('메세지 고정')
@@ -6,17 +6,7 @@ module.exports = {
 
     async execute(interaction){
       const msg = interaction.targetMessage;
-      const avatarURL  = msg.author.displayAvatarURL({ format: 'png', dynamic: true, size: 2048 });
       msg.pin();
-      const embed = new EmbedBuilder()
-	      .setColor('FFFFFF')
-        .setDescription(msg.content)
-        .setAuthor({ name: `${msg.author.username}`, iconURL: avatarURL })
-        .setThumbnail('https://i.ibb.co/t4V5qsf/star-icon.png')
-	      .addFields(
-          { name: `\u200B`, value: `메세지가 고정되었습니다. \nhttps://discord.com/channels/${msg.guild.id}/${msg.channelId}/${msg.id}` },
-        )
-        .setTimestamp();
-        await interaction.editReply({ embeds: [embed] });
+      await interaction.reply('메세지가 고정되었습니다.');
     }
 }
