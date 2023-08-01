51d85aa90647f8d051c7cc25e8da76e02890dd5f
SKSKIe <gahee3074@gmail.com>
2023-08-01 22:34:47 +0900

    고정

--- commands/unpin.js
+++ commands/unpin.js
@@ -1,12 +1,12 @@
 const { ContextMenuCommandBuilder, ApplicationCommandType } = require('discord.js');
 module.exports = {
 	  data: new ContextMenuCommandBuilder()
-      .setName('채팅 고정')
+      .setName('채팅 고정해제')
       .setType(ApplicationCommandType.Message),
 
     async execute(interaction){
       const msg = interaction.targetMessage;
-      msg.pin();
-      await interaction.reply('메세지가 고정되었습니다.');
+      msg.unpin();
+      await interaction.reply('메세지가 고정 해제되었습니다.');
     }
 }
